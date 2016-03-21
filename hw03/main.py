cc25e346d8db29deeedb998ef36c2c013f01c09d
az0977776 <az0977776@yahoo.com>
2016-03-21 02:23:25 -0400

    updated drawing

--- hw03/main.py
+++ hw03/main.py
@@ -11,14 +11,17 @@ transform = new_matrix()
 
 add_circle(edges, 250,250,0,50,0.001)
 draw_lines(edges,screen,color)
-add_circle(edges, 250,250,0,150,0.001)
+add_circle(edges, 250,250,0,75,0.001)
 draw_lines(edges,screen,color)
+add_circle(edges, 250,250,0,100,0.001)
+draw_lines(edges,screen,color)
+
 
 for mark in range(5):
     add_curve(shape, 30*mark, 150, 150, 50, 350, 150, 350, 300, 0.001, "b")
     draw_lines(shape,screen,color)
     add_curve(shape, 30*mark, 150, 150, 50, 350, 150, 350, 300, 0.001, "h")
-    matrix_mult(make_translate(20,0,0),shape)
+    matrix_mult(make_translate(0,-20,0),shape)
     draw_lines(shape,screen,color)
 
 display(screen)
