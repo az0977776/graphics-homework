6f53d41746e81611807b3249a16bc4e22026410c
Aaron Wang <az0977776@yahoo.com>
2016-03-29 18:34:36 -0400

    realized ident error

--- hw04/parser.py
+++ hw04/parser.py
@@ -23,45 +23,45 @@ def parse_file( f, points, transform, screen, color ):
                 args[i] = float( args[i] )
                 i+= 1
 
-                if cmd == 'line':
+            if cmd == 'line':
                     add_edge( points, args[0], args[1], args[2], args[3], args[4], args[5] )
                     
-                elif cmd == 'box':
-                    add_box( points, args[0], args[1], args[2], args[3], args[4], args[5] )
-                elif cmd == 'circle':
-                    add_circle( points, args[0], args[1], 0, args[2], .01 )
-                    
-                elif cmd == 'bezier':
-                    add_curve( points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], .01, 'bezier' )
-                    
-                elif cmd == 'hermite':
-                    add_curve( points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], .01, 'hermite' )
-                    
-                elif cmd == 'scale':
-                    s = make_scale( args[0], args[1], args[2] )
-                    matrix_mult( s, transform )
-                    
-                elif cmd == 'translate':
-                    t = make_translate( args[0], args[1], args[2] )
-                    matrix_mult( t, transform )
-                    
-                #angle = args[0] * ( math.pi / 180 )
-                elif cmd == 'xrotate':
-                    r = make_rotX( args[0] )
-                    matrix_mult( r, transform )
-                elif cmd == 'yrotate':
-                    r = make_rotY( args[0] )
-                    matrix_mult( r, transform )
-                elif cmd == 'zrotate':
-                    r = make_rotZ( args[0] )
-                    matrix_mult( r, transform )
-
-                elif cmd == 'box':
-                    add_box(points, args[0], args[1], args[2], args[3], args[4], args[5])
-                elif cmd == 'sphere':
-                    add_sphere(points, args[0], args[1], args[2], args[3], 0.01)
-                elif cmd == 'torus':
-                    add_torus(points, args[0], args[1], args[2], args[3], args[4], 0.01)
+            elif cmd == 'box':
+                add_box( points, args[0], args[1], args[2], args[3], args[4], args[5] )
+            elif cmd == 'circle':
+                add_circle( points, args[0], args[1], 0, args[2], .01 )
+                
+            elif cmd == 'bezier':
+                add_curve( points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], .01, 'bezier' )
+                
+            elif cmd == 'hermite':
+                add_curve( points, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], .01, 'hermite' )
+                
+            elif cmd == 'scale':
+                s = make_scale( args[0], args[1], args[2] )
+                matrix_mult( s, transform )
+                
+            elif cmd == 'translate':
+                t = make_translate( args[0], args[1], args[2] )
+                matrix_mult( t, transform )
+                
+            #angle = args[0] * ( math.pi / 180 )
+            elif cmd == 'xrotate':
+                r = make_rotX( args[0] )
+                matrix_mult( r, transform )
+            elif cmd == 'yrotate':
+                r = make_rotY( args[0] )
+                matrix_mult( r, transform )
+            elif cmd == 'zrotate':
+                r = make_rotZ( args[0] )
+                matrix_mult( r, transform )
+                
+            elif cmd == 'box':
+                add_box(points, args[0], args[1], args[2], args[3], args[4], args[5])
+            elif cmd == 'sphere':
+                add_sphere(points, args[0], args[1], args[2], args[3], 0.01)
+            elif cmd == 'torus':
+                add_torus(points, args[0], args[1], args[2], args[3], args[4], 0.01)
         else:
             if cmd == 'ident':
                 ident( transform )
