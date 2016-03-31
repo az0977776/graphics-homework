09e76d7a376643e4737c16977d2d58ba00000149
Aaron Wang <az0977776@yahoo.com>
2016-03-31 11:45:37 -0400

    update

--- hw05/draw.py
+++ hw05/draw.py
@@ -5,40 +5,78 @@ import math
 MAX_STEPS = 100
 
 def add_polygon( points, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
-    pass
+    points.append([x0,y0,z0,1])
+    points.append([x1,y1,z1,1])
+    points.append([x2,y2,z2,1])
 
 def draw_polygons( points, screen, color ):
-    pass
+    if len( points ) < 3:
+        print "Need at least 3 points to draw a triangle"
+        
+    p = 0
+    while p < len( points ) - 1:
+        draw_line( screen, points[p][0], points[p][1],
+                   points[p+1][0], points[p+1][1], color )
+        draw_line( screen, points[p+1][0], points[p+1][1],
+                   points[p+2][0], points[p+2][1], color )
+        draw_line( screen, points[p+2][0], points[p+2][1],
+                   points[p][0], points[p][1], color )        
+        p+= 3
 
 def add_box( points, x, y, z, width, height, depth ):
     x1 = x + width
-    y1 = y - height
+    y1 = y + height
     z1 = z - depth
 
-    add_edge( points, 
-              x, y, z, 
-              x, y, z )
-    add_edge( points, 
-              x, y1, z, 
-              x, y1, z )
-    add_edge( points, 
-              x1, y, z, 
-              x1, y, z )
-    add_edge( points, 
-              x1, y1, z, 
-              x1, y1, z )
-    add_edge( points, 
-              x, y, z1, 
-              x, y, z1 )
-    add_edge( points, 
-              x, y1, z1, 
-              x, y1, z1 )
-    add_edge( points, 
-              x1, y, z1, 
-              x1, y, z1 )
-    add_edge( points, 
-              x1, y1, z1, 
-              x1, y1, z1 )
+    add_polygon(points,
+                x,y,z,
+                x,y1,z,
+                x1,y,z)
+    add_polygon(points,
+                x1,y,z,
+                x,y1,z,
+                x1,y1,z)
+    add_polygon(points,
+                x1,y,z,
+                x1,y1,z,
+                x1,y,z1)
+    add_polygon(points,
+                x1,y1,z,
+                x1,y1,z1,
+                x1,y,z1)
+    add_polygon(points,
+                x1,y,z1,
+                x1,y1,z1,
+                x,y,z1)
+    add_polygon(points,
+                x,y,z1,
+                x1,y1,z1,
+                x,y1,z1)
+    add_polygon(points,
+                x,y,z1,
+                x,y1,z1,
+                x,y,z)
+    add_polygon(points,
+                x,y,z,
+                x,y1,z1,
+                x,y1,z)
+    add_polygon(points,
+                x,y,z1,
+                x,y,z,
+                x1,y,z1)
+    add_polygon(points,
+                x1,y,z1,
+                x,y,z,
+                x1,y,z)
+    add_polygon(points,
+                x,y1,z,
+                x,y1,z1,
+                x1,y1,z)
+    add_polygon(points,
+                x1,y1,z,
+                x,y1,z1,
+                x1,y1,z1)
+
 
 def add_sphere( points, cx, cy, cz, r, step ):
     
--- hw05/main.py
+++ hw05/main.py
@@ -2,6 +2,7 @@ from display import *
 from draw import *
 from parser import *
 from matrix import *
+from math import *
 import sys
 
 screen = new_screen()
@@ -9,8 +10,19 @@ color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 
-if len(sys.argv) == 2:
-    f = open(sys.argv[1])
+#add_polygon(edges,100,100,0,200,100,0,100,200,0)
+#add_polygon(edges,300,300,0,300,400,0,400,400,0)
+add_box(edges,100,100,100,100,150,200)
+print_matrix(edges)
+matrix_mult(make_translate(50,50,50),edges)
+matrix_mult(make_rotY(30.0/180*pi),edges)
+draw_polygons(edges,screen,color)
 
-parse_file( f, edges, transform, screen, color )
-f.close()
+display(screen)
+
+
+# if len(sys.argv) == 2:
+#     f = open(sys.argv[1])
+
+# parse_file( f, edges, transform, screen, color )
+# f.close()
