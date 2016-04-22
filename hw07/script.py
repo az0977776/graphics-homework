21ca165865401b47b1b02ab5110b38de4dbc4a6c
Aaron Wang <az0977776@yahoo.com>
2016-04-22 11:46:50 -0400

    update

--- hw07/script.py
+++ hw07/script.py
@@ -3,6 +3,8 @@ from display import *
 from matrix import *
 from draw import *
 
+ARG_COMMANDS = [ 'line', 'scale', 'translate', 'xrotate', 'yrotate', 'zrotate', 'circle', 'bezier', 'hermite', 'sphere', 'box', 'torus']
+
 def run(filename):
     """
     This function runs an mdl script
@@ -11,10 +13,17 @@ def run(filename):
     tmp = new_matrix()
     ident( tmp )
 
+    stack = []
+    stack += [tmp]
+    
     p = mdl.parseFile(filename)
-
+    print p[0]
+    
+    c = 0
     if p:
         (commands, symbols) = p
+        print commands
+        print symbols
     else:
         print "Parsing failed."
         return
@@ -23,4 +32,5 @@ def run(filename):
     screen = new_screen()
         
     for command in commands:
-        print command
+        pass
+        #print command
